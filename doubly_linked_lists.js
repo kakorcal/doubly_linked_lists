4d49bbe26449ce40abff0b52d763d45a323db959
Kenneth Korcal <kakorcal@gmail.com>
2016-06-22 06:17:49 -0700

    adding methods. verify mocha runs

--- doubly_linked_lists.js
+++ doubly_linked_lists.js
@@ -10,4 +10,48 @@ function DoublyLinkedList() {
   this.length = 0;
 }
 
+DoublyLinkedList.prototype.getNodeAt = function(index){
+
+};
+
+DoublyLinkedList.prototype.clear = function(){
+
+};
+
+DoublyLinkedList.prototype.push = function(){
+
+};
+
+DoublyLinkedList.prototype.pop = function(){
+
+};
+
+DoublyLinkedList.prototype.unshift = function(){
+
+};
+
+DoublyLinkedList.prototype.shift = function(){
+
+};
+
+DoublyLinkedList.prototype.get = function(index){
+
+};
+
+DoublyLinkedList.prototype.set = function(index, val){
+
+};
+
+DoublyLinkedList.prototype.insert = function(index, val){
+
+};
+
+DoublyLinkedList.prototype.remove = function(index){
+
+};
+
+DoublyLinkedList.prototype.reverse = function(){
+
+};
+
 module.exports = DoublyLinkedList;
